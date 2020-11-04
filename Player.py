c55e6fdd49361dacfc541cc466c0e95684a64587
ehuppe <ehuppe@potomacschool.org>
2020-11-04 11:55:06 -0700

    Commit4
    Outlining some methods in Player

--- Player.py
+++ Player.py
@@ -3,7 +3,35 @@ class Player:
     def __init__(self, t):
         self.type = t
 
-    def placeShips(self):
+    def spacesOfShip(self, ship):  # helper method to determine the number of spaces each ship occupies
+        if (ship == "D" | ship == "Destroyer"):
+            return 2
+        elif (ship == "C" | ship == "Cruiser"):
+            return 3
+        elif (ship == "S" | ship == "Submarine"):
+            return 3
+        elif (ship == "B" | ship == "Battleship"):
+            return 4
+        elif (ship == "A" | ship == "Aircraft Carrier"):
+            return 5
+
+    def placeShips(self, ship):  # places ships
+        spaces = self.spacesOfShip(ship)
+        xCoor = 0
+        yCoor = 0
+        bool = True
+        while(bool):
+            if (self.type == "human"):
+                # user input
+            else:
+                # generate random numbers within grid
+
+            if(#ship does not fit):
+                print("Error, ship does not fit")
+            else:
+                bool = False
+        # place ships here 
+
 
     def makeMoves(self):
 
